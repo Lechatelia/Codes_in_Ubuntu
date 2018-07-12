22b550fd924c8a9517801018c61d4569f3deea56
lechatelia <1078380439@qq.com>
2018-07-12 22:39:32 +0800

    add spi

--- SPI_test/main.cpp
+++ SPI_test/main.cpp
@@ -2,84 +2,85 @@
 #include<string>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include<string>
 
 //#include stdlib.h
 //#include stdio.h
 //#include string.h
-//#include unistd.h
-
+#include <unistd.h>
 #include <fcntl.h>
 //define O_WRONLY and O_RDONLY
 
 using namespace std;
 
-#define SYSFS_GPIO_EXPORT           "/sys/class/gpio/export"
+#define GPIO_EXPORT           "/sys/class/gpio/export"
+#define GPIO_Number_1      "1"
+#define GPIO_Number_2      "2"
+#define GPIO_Number_3      "3"
+#define GPIO_Number_4      "4"
 
-#define GPIO_Number      "117"
+#define GPIO_DIR_1          "/sys/class/gpio/gpio1/direction"
+#define GPIO_DIR_2          "/sys/class/gpio/gpio2/direction"
+#define GPIO_DIR_3          "/sys/class/gpio/gpio3/direction"
+#define GPIO_DIR_4          "/sys/class/gpio/gpio4/direction"
 
-#define SYSFS_GPIO_RST_DIR          "/sys/class/gpio/gpio117/direction"
+#define GPIO_VALUE_1          "/sys/class/gpio/gpio1/value"
+#define GPIO_VALUE_2          "/sys/class/gpio/gpio2/value"
+#define GPIO_VALUE_3          "/sys/class/gpio/gpio3/value"
+#define GPIO_VALUE_4          "/sys/class/gpio/gpio4/value"
 
 #define GPIO_DIR_OUT      "OUT"
-#define GPIO_DIR_IN      "OUT"
+#define GPIO_DIR_IN      "IN"
+
 #define GPIO_H      "1"
 #define GPIO_L      "0"
 
-#define SYSFS_GPIO_VAL          "/sys/class/gpio/gpio117/value"
 
 
 
+int GPIO_export(const char* export_name,const char* gpio_name)
+{
+         int fd = open(export_name, O_WRONLY);
 
-int main()
+         if(fd == -1)
 
+         {
+
+                   cout<<("ERR:export GPIO error.\n");
+
+                   return -1;
+
+         }
+
+         int re= write(fd, gpio_name ,sizeof(gpio_name));
+
+         close(fd);
+         return re;
+}
+
+int GPIO_set_direction(const char* gpio_dir_name,const char* dir)
 {
 
-    int fd;
-
-
-
-         //打开端口/sys/class/gpio# echo 117 > export
-
-         fd = open(SYSFS_GPIO_EXPORT, O_WRONLY);
+         int fd = open(gpio_dir_name, O_WRONLY);
 
          if(fd == -1)
 
          {
 
-                   cout<<("ERR: Radio hard reset pin open error.\n");
+                   cout<<("ERR: set pin direction open error.\n");
 
                    return -1;
 
          }
 
-         write(fd, GPIO_Number ,sizeof(GPIO_Number));
+        int re= write(fd, dir, sizeof(dir));
+        close(fd);
+        return re;
+}
 
-         close(fd);
-
-
-
-         //设置端口方向/sys/class/gpio/gpio117# echo out > direction
-
-         fd = open(SYSFS_GPIO_RST_DIR, O_WRONLY);
-
-         if(fd == -1)
-
-         {
-
-                   cout<<("ERR: Radio hard reset pin direction open error.\n");
-
-                   return -1;
-
-         }
-
-         write(fd, GPIO_DIR_OUT, sizeof(GPIO_DIR_OUT));
-
-         close(fd);
-
-
-
-         //输出复位信号: 拉高>100ns
-
-         fd = open(SYSFS_GPIO_VAL, O_RDWR);
+int GPIO_Set_level(const char* gpio_value_name,const char* level)
+{
+         int fd = open(gpio_value_name, O_RDWR);
 
          if(fd == -1)
 
@@ -90,37 +91,89 @@ int main()
                    return -1;
 
          }
+          int re=write(fd, level, sizeof(level));
+          close(fd);
+          return re;
+
+}
+
+int GPIO_read_level(const char* gpio_value_name,char* level)
+{
+         int fd = open(gpio_value_name, O_RDWR);
+
+         if(fd == -1)
+
+         {
+
+                   cout<<("ERR: Radio hard reset pin value open error.\n");
+
+                   return -1;
+
+         }
+          int re=read(fd, level, sizeof(level));
+          close(fd);
+          return re;
+
+}
+
+
+
+int main()
+
+{
+
+
+
+         //打开端口/sys/class/gpio# echo 117 > export
+
+         for(int i=0;i<4;i++)
+         {
+            GPIO_export(GPIO_EXPORT,GPIO_Number_1);
+            GPIO_export(GPIO_EXPORT,GPIO_Number_2);
+            GPIO_export(GPIO_EXPORT,GPIO_Number_3);
+            GPIO_export(GPIO_EXPORT,GPIO_Number_4);
+         }
+
+
+
+
+         //设置端口方向/sys/class/gpio/gpio117# echo out > direction
+
+         for(int i=0;i<4;i++)
+         {
+            GPIO_set_direction(GPIO_DIR_1,GPIO_DIR_IN);
+            GPIO_set_direction(GPIO_DIR_2,GPIO_DIR_OUT);
+            GPIO_set_direction(GPIO_DIR_3,GPIO_DIR_OUT);
+            GPIO_set_direction(GPIO_DIR_4,GPIO_DIR_OUT);
+         }
+
+
+
+         //输出复位信号: 拉高>100ns
+
 
          while(1)
 
          {
 
-                   write(fd, GPIO_H, sizeof(GPIO_H));
-
-                   usleep(1000000);
-
-                   write(fd, GPIO_L, sizeof(GPIO_L));
-
-                   usleep(1000000);
+            //GPIO_Set_level(GPIO_VALUE_1,GPIO_H)
+            GPIO_Set_level(GPIO_VALUE_2,GPIO_H);
+            GPIO_Set_level(GPIO_VALUE_3,GPIO_H);
+            GPIO_Set_level(GPIO_VALUE_4,GPIO_H);
+            usleep(1000);
+            GPIO_Set_level(GPIO_VALUE_2,GPIO_L);
+            GPIO_Set_level(GPIO_VALUE_3,GPIO_L);
+            GPIO_Set_level(GPIO_VALUE_4,GPIO_L);
+            usleep(1000);
 
          }
 
-         close(fd);
 
 
 
-         cout<<("INFO: Radio hard reset pin value open error.\n");
 
          return 0;
 
 
 
 }
-
-/*int main()
-{
-    string str1;
-    cout<<"input"<<endl;
-    cin>>str1;
-    cout<<str1<<endl;
-}*/
